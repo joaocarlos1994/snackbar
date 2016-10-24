4a976697024fcf1637a525af5219b6e39a8977eb
João Carlos Batista <joao_nad@hotmail.com>
2016-10-24 12:11:58 -0200

    Nova config spring security.

---
+++ src/main/java/authentication/PreAuthenticatedUserFilter.java
@@ -0,0 +1,64 @@
+/*
+ * @(#)PreAuthenticatedUserFilter.java 1.0 04/12/2015
+ *
+ * Copyright (c) 2015, Embraer. All rights reserved. Embraer S/A
+ * proprietary/confidential. Use is subject to license terms.
+ */
+
+package authentication;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+/**
+ * A classe <code>PreAuthenticatedUserFilter</code> é o filtro responsável por
+ * recuperar o token do usuário no header de uma requisição e retorná-lo, para
+ * que posteriormente as implementações de <code>AuthenticationManager</code> de
+ * cada aplicação possam recuperar os dados do usuário no Embraer Account, a
+ * partir do token, e colocar o usuário no contexto de segurança do Spring.
+ * 
+ * Caso o filtro verifique a ausência do token no header da requisição, será
+ * retornado no response o HttpStatus com o código 403.
+ *
+ * @author Roberto Perillo
+ * @version 1.0 04/12/2015
+ */
+public class PreAuthenticatedUserFilter extends AbstractPreAuthenticatedProcessingFilter {
+
+    private static final String AUTHORIZATION = "Authorization";
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getPreAuthenticatedPrincipal(final HttpServletRequest request) {
+        return request.getHeader(AUTHORIZATION) != null ? (String) request.getHeader(AUTHORIZATION) : request.getParameter(AUTHORIZATION);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected Object getPreAuthenticatedCredentials(final HttpServletRequest request) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        try {
+            super.doFilter(request, response, chain);
+        } finally {
+            SecurityContextHolder.clearContext();
+           // final HttpSession session = httpServletRequest.getSession(false);
+           // if (session != null) {
+           //     session.removeAttribute("SPRING_SECURITY_CONTEXT");
+           // }
+            //TokenRepository.clear();
+        }
+    }
+}
--- src/main/java/br/com/hyperclass/snackbar/config/SecurityConfiguration.java
+++ src/main/java/br/com/hyperclass/snackbar/config/SecurityConfiguration.java
@@ -6,13 +6,26 @@
  */
 package br.com.hyperclass.snackbar.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import authentication.PreAuthenticatedUserFilter;
 import br.com.hyperclass.snackbar.infrastructure.repository.UserRepository;
 
 /**
@@ -22,27 +35,65 @@ import br.com.hyperclass.snackbar.infrastructure.repository.UserRepository;
  * @version 1.0 22 de out de 2016
  */
 @EnableWebSecurity
-@ComponentScan(basePackages = {"br.com.hyperclass.snackbar.infrastructure"})
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@ComponentScan(basePackages = { "br.com.hyperclass.snackbar.infrastructure" })
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	@Qualifier("jwtAuthenticationManager")
+	private AuthenticationManager jwtAuthenticationManager;
+
+	@Autowired
+	@Qualifier("providerManager")
+	private AuthenticationManager providerManager;
+
+	@Autowired
+	private AuthenticationSuccessHandler successHandler;
+
+	@Autowired
+	private AuthenticationFailureHandler failureHandler;
 
 	/**
 	 * Method reponsability for authentic and authority
-	 * */
+	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/**").permitAll()
-		.and().formLogin();
+		http.authorizeRequests().antMatchers("/**").permitAll().and().formLogin();
 	}
-	
+
 	/**
-	 * Esse método adiciona um novo usuário no contexto do Spring Security
-	 * This method add one new user in contex Spring Security
-	 * */
+	 * Esse método adiciona um novo usuário no contexto do Spring Security This
+	 * method add one new user in contex Spring Security
+	 */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(new UserRepository()).
-		passwordEncoder(new BCryptPasswordEncoder());
+		auth.userDetailsService(new UserRepository()).passwordEncoder(new BCryptPasswordEncoder());
 	}
-	
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder(5);
+	}
+
+	@Bean
+	public Filter preAuthenticationFilter() {
+		final PreAuthenticatedUserFilter filter = new PreAuthenticatedUserFilter();
+		filter.setAuthenticationManager(jwtAuthenticationManager);
+		return filter;
+	}
+
+	@Bean
+	public Filter loginFilter() {
+		final UsernamePasswordAuthenticationFilter filter = new UsernamePasswordAuthenticationFilter();
+		filter.setAuthenticationManager(providerManager);
+		filter.setAuthenticationSuccessHandler(successHandler);
+		filter.setAuthenticationFailureHandler(failureHandler);
+		return filter;
+	}
+
+	@Bean
+	public Filter anonymousFilter() {
+		return new AnonymousAuthenticationFilter("anonymousUser");
+	}
+
 }
